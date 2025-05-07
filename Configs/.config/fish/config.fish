e9694bf0ba521325ce1ffdc082628e2eb92da078
Void Strike <kaydzyu1508@gmail.com>
2025-05-07 15:13:35 +0500

    ...

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -42,12 +42,26 @@ source ~/.config/fish/hyde_config.fish
 function fish_prompt
     set -l ip (ip -4 addr show | grep -v '127.0.0.1' | grep -v 'secondary' | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -z 's/\n/|/g;s/|\$/\n/' | rev | cut -c 2- | rev)
     set -l user (whoami)
-    set -l cwd (pwd)
+    set -l cwd (prompt_pwd)
+
+    # Git info
+    set -l git_branch (command git symbolic-ref --short HEAD ^/dev/null 2>/dev/null)
+    set -l git_dirty (command git status --porcelain ^/dev/null 2>/dev/null)
+    set -l git_display ""
+    if test -n "$git_branch"
+        if test -n "$git_dirty"
+            set git_display (set_color brblue)"{🌿 $git_branch"(set_color red)" ✖️}"(set_color normal)
+        else
+            set git_display (set_color brblue)"{🌿 $git_branch"(set_color green)" ✔️}"(set_color normal)
+        end
+    end
 
     if string match -r ".*/dev/tty.*" (tty)
-        echo -e "\e[1;32m[HQ:\e[1;31m$ip | $user\e[1;32m]\n[>]\e[1;36m$cwd \$ \e[0m"
+        echo -e (set_color green)"[HQ:"(set_color red)"$ip🔥$user"(set_color green)"]$git_display"
+        echo -e (set_color cyan)"[>]"(set_color cyan)"$cwd \$ "(set_color normal)
     else
-        echo -e "\e[1;32m┌──[HQ🚀🌐\e[1;31m$ip🔥$user\e[1;32m]\n└──╼[👾]\e[1;36m$cwd \$ \e[0m"
+        echo -e (set_color green)"┌──[HQ🚀🌐"(set_color red)"$ip🔥$user"(set_color green)"]$git_display"
+        echo -e (set_color green)"└──╼[👾]"(set_color cyan)"$cwd \$ "(set_color normal)
     end
 end
 
